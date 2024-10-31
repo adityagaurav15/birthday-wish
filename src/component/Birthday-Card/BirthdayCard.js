07ef8e122bea6a7f9d338064c6548e0d46e4a549
Aditya Gaurav <adityagaurav15898@gmail.com>
2024-10-31 23:59:11 +0530

    card background

--- src/component/Birthday-Card/BirthdayCard.js
+++ src/component/Birthday-Card/BirthdayCard.js
@@ -4,16 +4,16 @@ import imageCard from "../../assets/birthdayInner.jpeg";
 import birthdayCard from "../../assets/birthdayCard.jpg";
 
 const FireWork = () => {
-  const [cardClass, setCardClass] = useState("");
+  const [cardclassName, setCardclassName] = useState("");
   const timerRef = useRef(null);
   const audioRef = useRef(null);
 
   // Function to handle the card opening
   const openCard = () => {
-    setCardClass("open-half");
+    setCardclassName("open-half");
     if (timerRef.current) clearTimeout(timerRef.current);
     timerRef.current = setTimeout(() => {
-      setCardClass("open-fully");
+      setCardclassName("open-fully");
       // Only play audio if the user has interacted
       if (audioRef.current) {
         audioRef.current.play().catch((error) => {
@@ -26,10 +26,10 @@ const FireWork = () => {
 
   // Function to handle the card closing
   const closeCard = () => {
-    setCardClass("close-half");
+    setCardclassName("close-half");
     if (timerRef.current) clearTimeout(timerRef.current);
     timerRef.current = setTimeout(() => {
-      setCardClass("");
+      setCardclassName("");
       // Pause the audio if it's playing
       if (audioRef.current && !audioRef.current.paused) {
         audioRef.current.pause();
@@ -59,25 +59,27 @@ const FireWork = () => {
   }, []);
 
   return (
-    <div class="birthday-card">
-      <div class="cover-inner">
-        <img src={imageCard} width="100%" />
-      </div>
+    <div className="background-card">
+      <div className="birthday-card">
+        <div className="cover-inner">
+          <img src={imageCard} width="100%" />
+        </div>
 
-      <div class="cover-outer">
-        <img src={birthdayCard} width="100%" />
-      </div>
+        <div className="cover-outer">
+          <img src={birthdayCard} width="100%" />
+        </div>
 
-      <div class="text">
-        <p class="title">
-          Happy Birthday! <br /> Saloni
-        </p>
-        <p>
-          "I was going to get you something amazing, but then I remembered—you
-          already have me. You're welcome! 😉 Jokes aside, may your day be
-          filled with laughter, cake, and lots of love. <br />
-          Once again, happiest birthday, Saloni! ❤️"
-        </p>
+        <div className="text">
+          <p className="title">
+            Happy Birthday! <br /> Saloni
+          </p>
+          <p>
+            "I was going to get you something amazing, but then I remembered—you
+            already have me. You're welcome! 😉 Jokes aside, may your day be
+            filled with laughter, cake, and lots of love. <br />
+            Once again, happiest birthday, Saloni! ❤️"
+          </p>
+        </div>
       </div>
     </div>
   );
