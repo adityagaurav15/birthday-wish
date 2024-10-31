62d2835fbcd6d5d752bc2ce00a2a8f99c223633b
Aditya Gaurav <adityagaurav15898@gmail.com>
2024-11-01 00:07:41 +0530

    changes

--- src/component/Birthday-Card/BirthdayCard.js
+++ src/component/Birthday-Card/BirthdayCard.js
@@ -1,63 +1,9 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import "./BirthdayCard.css";
 import imageCard from "../../assets/birthdayInner.jpeg";
 import birthdayCard from "../../assets/birthdayCard.jpg";
 
-const FireWork = () => {
-  const [cardclassName, setCardclassName] = useState("");
-  const timerRef = useRef(null);
-  const audioRef = useRef(null);
-
-  // Function to handle the card opening
-  const openCard = () => {
-    setCardclassName("open-half");
-    if (timerRef.current) clearTimeout(timerRef.current);
-    timerRef.current = setTimeout(() => {
-      setCardclassName("open-fully");
-      // Only play audio if the user has interacted
-      if (audioRef.current) {
-        audioRef.current.play().catch((error) => {
-          console.error("Error playing audio:", error);
-        });
-      }
-      timerRef.current = null;
-    }, 1000);
-  };
-
-  // Function to handle the card closing
-  const closeCard = () => {
-    setCardclassName("close-half");
-    if (timerRef.current) clearTimeout(timerRef.current);
-    timerRef.current = setTimeout(() => {
-      setCardclassName("");
-      // Pause the audio if it's playing
-      if (audioRef.current && !audioRef.current.paused) {
-        audioRef.current.pause();
-      }
-      timerRef.current = null;
-    }, 1000);
-  };
-
-  // Check if the audio can play
-  useEffect(() => {
-    const audioElement = audioRef.current;
-    const handleCanPlay = () => {
-      console.log("Audio is ready to play");
-    };
-
-    // Add canplaythrough event listener
-    if (audioElement) {
-      audioElement.addEventListener("canplaythrough", handleCanPlay);
-    }
-
-    // Cleanup event listener on component unmount
-    return () => {
-      if (audioElement) {
-        audioElement.removeEventListener("canplaythrough", handleCanPlay);
-      }
-    };
-  }, []);
-
+const BirthdayCard = () => {
   return (
     <div className="background-card">
       <div className="birthday-card">
@@ -85,4 +31,4 @@ const FireWork = () => {
   );
 };
 
-export default FireWork;
+export default BirthdayCard;
